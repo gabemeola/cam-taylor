5030618d78042d4f3a311af4175c84ddd1f8a552
gabemeola <gabemeola@gmail.com>
2015-12-21 00:52:19 -0700

    added animation to the expanded bar

--- public/bundle.js
+++ public/bundle.js
@@ -116,7 +116,18 @@
 		
 		$scope.uiviewOpen = function(init) {
 			if(init){
-				$scope.uiview = true;
+				if($scope.uiview == true){
+					$scope.uiview = false;
+					document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-closeanimation');
+					setTimeout(
+						function() {
+							$scope.uiview = true;
+							document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-openanimtion');
+					}, 500);
+				} else {
+					$scope.uiview = true;
+					document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-openanimtion');
+				}
 			}	
 		};
 		
--- public/js/ctMain.js
+++ public/js/ctMain.js
@@ -3,7 +3,18 @@ angular.module("camtaylorApp")
 	
 	$scope.uiviewOpen = function(init) {
 		if(init){
-			$scope.uiview = true;
+			if($scope.uiview == true){
+				$scope.uiview = false;
+				document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-closeanimation');
+				setTimeout(
+					function() {
+						$scope.uiview = true;
+						document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-openanimtion');
+				}, 500);
+			} else {
+				$scope.uiview = true;
+				document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-openanimtion');
+			}
 		}	
 	};
 	
