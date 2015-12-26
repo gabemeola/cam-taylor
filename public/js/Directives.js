480eff9871189d5f48ea99a42f43c87c2a7ed6dd
gabemeola <gabemeola@gmail.com>
2015-12-26 11:12:40 -0700

    added a countdown timer to wedding date

--- public/js/Directives.js
+++ public/js/Directives.js
@@ -4,3 +4,8 @@ angular.module("camtaylorApp")
 		templateUrl: "./templates/_card-content.html"				 
 	};			 
 })
+.directive("pictures", function(){
+	return {
+		templateUrl: "./templates/_pictures.html"
+	};
+});
--- public/js/ctMain.js
+++ public/js/ctMain.js
@@ -1,5 +1,6 @@
 angular.module("camtaylorApp")
-.controller("ctMain", function($scope){
+.controller("ctMain", function($scope, $q){
+	countdownClock();
 	
 	$scope.uiviewOpen = function(init) {
 		if(init){
@@ -18,6 +19,30 @@ angular.module("camtaylorApp")
 		}	
 	};
 	
-	$scope.guests = 0;
+	$scope.rsvpGuests = 0;
 	
+	
+	function countdownClock(){
+		var end = new Date('03/22/2016 10:1 AM'),
+    		_second = 1000,
+    		_minute = _second * 60,
+    		_hour = _minute * 60,
+    		_day = _hour * 24,
+    		timer;
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+            clearInterval(timer);
+            document.getElementById('countdown').innerHTML = 'EXPIRED!';
+            return;
+        }
+        var days = Math.floor(distance / _day),
+        		hours = Math.floor((distance % _day) / _hour),
+        		minutes = Math.floor((distance % _hour) / _minute),
+        		seconds = Math.floor((distance % _minute) / _second);
+
+        document.getElementById('countdown').innerHTML = days + ' Days!';
+    }
+	}
 });
