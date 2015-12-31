69c4f4313d771be1712e97512c8a1280853a0681
gabemeola <gabemeola@gmail.com>
2015-12-31 15:13:41 -0700

    remove animation from card--expanded, to add ng-view animation

--- public/js/ctMain.js
+++ public/js/ctMain.js
@@ -4,20 +4,10 @@ angular.module("camtaylorApp")
 	
 	$scope.uiviewOpen = function(init) {
 		if(init){
-			if($scope.uiview == true){
-				$scope.uiview = false;
-				document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-closeanimation');
-				setTimeout(
-					function() {
-						$scope.uiview = true;
-						document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-openanimtion');
-				}, 500);
-			} else {
-				$scope.uiview = true;
-				document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-openanimtion');
+			$scope.uiview = true;
+			document.getElementById('card--expanded').setAttribute('class', 'card--expanded card--expanded-openanimtion');
 			}
-		}	
-	};
+		};
 	
 	function countdownClock(){
 		var end = new Date('03/22/2016 10:1 AM'),
