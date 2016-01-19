4afd064cb2201fc0aa823a51b2b576c1913530de
gabemeola <gabemeola@gmail.com>
2016-01-19 11:55:27 -0700

    added admin

--- public/js/ctAdmin.js
+++ public/js/ctAdmin.js
@@ -1,5 +1,7 @@
 angular.module("camtaylorApp")
-.controller("ctAdmin", ["$scope", "svAdminLogin", "ftAuth", function($scope, svAdminLogin, ftAuth) {
+.controller("ctAdmin", ["$scope", "svAdminLogin", "ftAuth", "FIRE", "$q", function($scope, svAdminLogin, ftAuth, FIRE, $q) {
+	var ref = new Firebase(FIRE.url + "rsvps/"),
+			defer = $q.defer();
 	
 	$scope.adminLogin = function() {
 		var username = $scope.admin.username,
@@ -10,4 +12,18 @@ angular.module("camtaylorApp")
 	ftAuth.$onAuth(function(authData) {
 		$scope.authData = authData;
 	});
+	
+	var grabRsvp = function(){
+		ref.on("value", function(snapshot) {
+			var data = snapshot.val();
+			defer.resolve(data);
+		})
+		return defer.promise;
+	}
+	
+	grabRsvp().then(function(res) {
+		$scope.rsvpData = res;
+		console.log($scope.rsvpData)
+	});
+									
 }]);
