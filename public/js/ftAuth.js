d829c13c7bf4ca23d6fa3d5215988bbce673d0da
gabemeola <gabemeola@gmail.com>
2016-04-24 23:09:02 -0600

    llll

--- public/js/ftAuth.js
+++
@@ -1,5 +0,0 @@
-angular.module("camtaylorApp")
-.factory("ftAuth", ["$q", "$firebaseAuth", "FIRE", function($q, $firebaseAuth, FIRE){
-	var ref = new Firebase(FIRE.url);
-  return $firebaseAuth(ref);
-}]);
